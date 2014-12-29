40052132133d7fc2701cfa089f81e3eaea6185e7
Lorenzo Milesi <maxxer@yetopen.it>
2014-12-29 14:02:29 +0100

    Zimbra monitoring scripts and template for Zabbix

---
+++ zabbix-zimbra/README.md
@@ -0,0 +1,20 @@
+Zimbra monitoring with Zabbix
+=============================
+
+This template offers services autodiscover, version tracking and service status.
+It's compatibile with Zimbra 8.6 (and its services with spaces).
+
+Please note the external script requires sudo privileges to run, so either copy the content of the sudo_zbx-zimbra.conf file to your sudoers 
+or link it to /etc/sudoers.d/.
+Also copy yo-zimbra.conf to /etc/zabbix/zabbix_agentd.conf.d/ and the script file to your zabbix agent's script directory (usually /etc/zabbix/scripts, if differeny adjust yo-zimbra.conf accordingly).
+
+Restart zabbix-agent and import the template to your server.
+The service discovery is set very long (1 day), so it may take very long for services to show up in Zabbix.
+
+
+The bash script was inspired by [blog.linuxnet.ch](https://blog.linuxnet.ch/zimbra-monitoring-with-zabbix/). 
+
+
+License & Copyright
+-------------------
+Code and documentation copyright 2014 YetOpen S.r.l.. Released under the GPLv3 license.
---
+++ zabbix-zimbra/zbx_zimbra.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+# Zabbix script to check Zimbra services and perform service discovery.
+# Supports Zimbra 8.6 and "two-worded" service names
+# Author: Lorenzo Milesi <maxxer@yetopen.it>
+# Copyright: YetOpen S.r.l. 2014
+# License: GPLv3
+
+# uncomment for debug
+#set -x
+
+COMMAND="sudo -u zimbra /opt/zimbra/bin/zmcontrol"
+
+case "$1" in
+    version) 
+    # Return zimbra version
+    VERS=$($COMMAND -v)
+    if [ $? -eq 0 ] ; then
+        echo $VERS
+        exit 0;
+    fi
+    # error
+    return 1;
+    ;;
+    discover) 
+    # Return a list of running services in JSON
+	echo "{"
+	echo "\t\"data\":[\n"
+    	SRVCS=$($COMMAND status | grep Running | awk '{$(NF--)=""; print}' | sed 's/^/\t{ \"{#ZIMBRASERVICE}\":\"/' | sed 's/\ $/\" },/')
+	# Remove last comma from the sting, to make a good JSON
+	echo $(echo $SRVCS | sed 's/,\+$//')
+	echo "\n\t]\n"
+	echo "}"
+    return 0;
+    ;;
+    *)
+    # move on...
+	check=$1
+
+	if [ "$check" = "" ]; then
+	  echo "No Zimbra service specified..."
+	  exit 1
+	fi
+
+	maxage=120
+	file='/var/run/zabbix/zimbra_status'
+
+	# Very basic concurrency check
+	x=0
+	while [ -f "$file.tmp" ]; do
+		sleep 5;
+		x=$((x+1))
+		# don't wait too long anyway, remove an eventually stale lock. Anyway we have 15s zabbix agent timeout
+		if [ $x -ge 3 ]; then
+			rm "$file.tmp";
+		fi
+	done
+	#check if cached status file size > 0
+	if [ -s ${file} ]; then 
+	  OLD=`stat -c %Z $file`
+	  NOW=`date +%s`
+
+	  # if older then maxage, update file
+	  if [ `expr $NOW - $OLD` -gt $maxage ]; then
+	    $COMMAND status > $file.tmp
+	    mv $file.tmp $file
+	  fi
+	else
+	    rm -f ${file}
+	    $COMMAND status > $file.tmp
+	    mv $file.tmp $file
+	fi
+
+	STATUS="$(cat $file | grep "$check" | awk '{print $NF}')"
+
+	if [ "$STATUS" != "Running" ]; then
+	  echo 0
+	else
+	  echo 1
+	fi
+    ;;
+esac
+
+exit 0;
+
